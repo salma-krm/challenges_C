f46b106974001b1341447343d32c5f1ec0eb6f18
sallma <salmatergui7@gmail.com>
2024-09-24 17:15:24 +0200

    cccccc

---
+++ variables/challenge11.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+int main() {
+    int lar, lon , s;
+    
+    printf("la valeur de longeur est :");
+    scanf("%d",&lon);
+    printf("la valeur de largeur est:");
+    scanf("%d",&lar);
+    s =lar*lon;
+    printf("surface :%d",s);
+    
+    
+    
+    
+
+    return 0;
+}
